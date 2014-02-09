53b219ba86ead891fcca73a251104aba475b5467
Jo Bridts <jo.bridts@chiro.be>
2014-02-09 01:12:52 +0000

    config

--- js/app.js
+++ js/app.js
@@ -64,6 +64,7 @@ angular.module('starter', ['ionic', 'starter.services', 'starter.controllers'])
 
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/tab/pets');
+  
 
 });
 
